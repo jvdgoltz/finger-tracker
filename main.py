d07eab5a5e83a70d4f91f3ece2f45e29071c815f
Julian von der Goltz <j.vdgoltz@gmail.com>
2019-01-21 10:12:05 +0100

    add main and create first code

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
+
+from PIL import Image
 import tensorflow as tf
 from tensorflow.python.keras import models, layers, losses, optimizers
 from tensorflow.python.keras.applications import MobileNetV2
 
+tf.enable_eager_execution()
 
 def construct_model():
     conv_base = MobileNetV2(input_shape=None,
@@ -25,15 +29,44 @@ def construct_model():
     return model
 
 
-def create_datasets(background_dir, open_dir, closed_dir):
-    background = tf.data.Dataset().from_tensor_slices(())
+def create_datasets(background_dir, closed_dir, open_dir, val_train_split):
+    bg_list = os.listdir(background_dir)
+    cl_list = os.listdir(closed_dir)
+    op_list = os.listdir(open_dir)
+    n_total = len(bg_list + cl_list + op_list)
 
-    return 1 ,2, 3, 4
+    def image_generator(img_list, img_dir):
+        for img_name in img_list:
+            yield Image.open(os.path.join(img_dir, img_name))
+
+    bg_data = tf.data.Dataset().from_generator(lambda: image_generator(bg_list, background_dir), tf.uint8)
+    cl_data = tf.data.Dataset().from_generator(lambda: image_generator(cl_list, closed_dir), tf.uint8)
+    op_data = tf.data.Dataset().from_generator(lambda: image_generator(op_list, open_dir), tf.uint8)
+
+    def parse_data_map_func(element, finger_status, face_status):
+        x = element
+        finger_status = tf.one_hot(finger_status, 3)
+        finger_positions = [0, 0, 0, 0]
+        face_positions = [0, 0, 0, 0]
+        return x, (finger_status, finger_positions, face_status, face_positions)
+
+    bg_data = bg_data.map(lambda el: parse_data_map_func(el, 0, 0))
+    cl_data = cl_data.map(lambda el: parse_data_map_func(el, 1, 1))
+    op_data = op_data.map(lambda el: parse_data_map_func(el, 2, 1))
+
+    all_data = bg_data.concatenate(cl_data).concatenate(op_data).shuffle(n_total)
+    n_train = int(val_train_split * n_total)
+    n_val = n_total-n_train
+    train_data = all_data.take(n_train)
+    val_data = all_data.skip(n_train)
+    return train_data, n_train, val_data, n_val
 
 
 def main():
 
-    train_data, steps_per_epoch, val_data, val_steps = create_datasets()
+    train_data, steps_per_epoch, val_data, val_steps = create_datasets("./data/background",
+                                                                       "./data/closed",
+                                                                       "./data/open")
 
     model = construct_model()
     model.summary()
@@ -48,4 +81,15 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    train_data, steps_per_epoch, val_data, val_steps = create_datasets("./data/background",
+                                                                       "./data/closed",
+                                                                       "./data/open",
+                                                                       0.1)
+
+    i = 0
+    for x, y in train_data:
+        i += 1
+        img = Image.fromarray(x.numpy())
+        img.show()
+        if i == 10:
+            break
